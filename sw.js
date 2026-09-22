f930570d3860fb671b967265aec680ff93646021
goldshines812-jpg <goldshines812@gmail.com>
2026-09-22 08:15:19 +0800

    Upgrade service worker cache to version 5
    Updated cache version and modified caching strategy.

--- sw.js
+++ sw.js
@@ -1,34 +1,22 @@
-const CACHE_NAME = "tissue-price-v4";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE_NAME = "tissue-price-v5";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+  self.skipWaiting();
 });
+
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request, { cache: "no-store" })
+      .catch(() => caches.match(event.request))
   );
 });
